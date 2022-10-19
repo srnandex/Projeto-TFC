bec114bbe5c31865b3ad3b33ff7850b3fae7c17d
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-10-19 17:51:57 -0300

    tentativa nao sei mais 2

--- app/backend/src/middlewares/errosM.ts
+++ app/backend/src/middlewares/errosM.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import CustomError from '../helpers/customError';
 
 class ErrorHandler {
