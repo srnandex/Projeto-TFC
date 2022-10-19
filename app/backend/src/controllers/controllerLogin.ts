4e9d44f08eb4bee229ce822c0ab55a4aa76b393e
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-10-19 19:55:40 -0300

    try cath me salvou

--- app/backend/src/controllers/controllerLogin.ts
+++ app/backend/src/controllers/controllerLogin.ts
@@ -1,10 +1,14 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import LoginService from '../services/serviceLogin';
 
 export default class LoginController {
-  static async login(req: Request, res: Response) {
-    const login = req.body;
-    const token = await LoginService.Login(login);
-    return res.status(200).json({ token });
+  static async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const login = req.body;
+      const token = await LoginService.Login(login);
+      return res.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
   }
 }
