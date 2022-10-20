c1b4ff966a5dddef4d03ba2c5d40516a354c2a7a
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-10-20 11:10:56 -0300

    passa testeeesss

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -4,7 +4,6 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
 import ILogin from '../interfaces/ILogin.interfaces';
