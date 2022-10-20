b5095dcaaa97b7fd73a647df0c5540b6dbd401e6
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-10-20 00:42:44 -0300

    requisito 14

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,27 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
