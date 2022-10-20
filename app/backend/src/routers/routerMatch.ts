42faee3744a13c1c1f53f97963e0dc8e67cb0495
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-10-20 06:45:53 -0300

    sou muito burro vamos ver se vai

--- app/backend/src/routers/routerMatch.ts
+++ app/backend/src/routers/routerMatch.ts
@@ -3,9 +3,9 @@ import MatchController from '../controllers/controllerMatchs';
 
 const matchRouter = express.Router();
 
-matchRouter.get('/', MatchController.getAll);
-matchRouter.post('/', MatchController.createMatch);
-matchRouter.patch('/:id', MatchController.updateMatchGoals);
-matchRouter.patch('/:id/finish', MatchController.updateProgressMatch);
+matchRouter.get('/matches', MatchController.getAll);
+matchRouter.post('/matches', MatchController.createMatch);
+matchRouter.patch('/matches/:id', MatchController.updateMatchGoals);
+matchRouter.patch('/matches/:id/finish', MatchController.updateProgressMatch);
 
 export default matchRouter;
